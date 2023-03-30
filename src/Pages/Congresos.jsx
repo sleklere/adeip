ae0aeca518d668d3fb2a9ba4b63ad8f3b93fa4b0
j-leklere <jleklere@yahoo.com.ar>
2023-03-30 18:56:40 -0300

    board of directors

--- src/Pages/Congresos.jsx
+++ src/Pages/Congresos.jsx
@@ -1,6 +1,7 @@
+import styles from "./Congresos.module.css";
+
 import PageLayoutTop from "../Components/PageLayoutTop";
 import backgroundImage from "../Assets/congress.jpg";
-import styles from "./Congresos.module.css";
 
 function Congresos() {
   return (
--- src/Pages/ConsejoDirectivo.jsx
+++ src/Pages/ConsejoDirectivo.jsx
@@ -1,5 +1,8 @@
 import styles from "./ConsejoDirectivo.module.css";
 
+import PageLayoutTop from "../Components/PageLayoutTop";
+import backgroundImage from "../Assets/consejo-directivo.jpg";
+
 import Presidenta from "../Assets/presidenta-adeip.jpeg";
 import VicepresidentaPrimera from "../Assets/vicepresidenta-primera-adeip.jpeg";
 import VicepresidenteSegundo from "../Assets/person.png";
@@ -7,9 +10,7 @@ import VicepresidenteSegundo from "../Assets/person.png";
 function ConsejoDirectivo() {
   return (
     <div className={styles.container}>
-      <div className={styles.carrousel}>
-        <h1 className={styles["heading-primary"]}>Consejo Directivo</h1>
-      </div>
+      <PageLayoutTop title={"CONSEJO DIRECTIVO"} image={backgroundImage} />
       <div className={styles.title}>
         <h2 className={styles["heading-secondary"]}>
           Consejo Directivo A.D.E.I.P 2022-2023
@@ -19,62 +20,65 @@ function ConsejoDirectivo() {
         <section className={styles["charges-primary"]}>
           <div className={styles.charge}>
             <img src={Presidenta} alt="" />
-            <p>Presidente</p>
-            <h3 className={styles["heading-tertiary"]}>María Elena Ocampo</h3>
+            <div className={styles["charge-info"]}>
+              <p>Presidenta</p>
+              <h3 className={styles["heading-tertiary"]}>María Elena Ocampo</h3>
+            </div>
           </div>
           <div className={styles.charge}>
             <img src={VicepresidentaPrimera} alt="" />
-            <p>Vicepresidenta 1ª</p>
-            <h3 className={styles["heading-tertiary"]}>Ana María Bernia</h3>
+            <div className={styles["charge-info"]}>
+              <p>Vicepresidenta 1ª</p>
+              <h3 className={styles["heading-tertiary"]}>Ana María Bernia</h3>
+            </div>
           </div>
           <div className={styles.charge}>
             <img src={VicepresidenteSegundo} alt="" />
-            <p>Vicepresidente 2º</p>
-            <h3 className={styles["heading-tertiary"]}>
-              Ernesto Federico Pais
-            </h3>
+            <div className={styles["charge-info"]}>
+              <p>Vicepresidente 2º</p>
+              <h3 className={styles["heading-tertiary"]}>
+                Ernesto Federico Pais
+              </h3>
+            </div>
           </div>
         </section>
         <section className={styles["charges-secondary"]}>
           <div>
-            <p>Secretaria</p>
-            <h3 className={styles["heading-tertiary"]}>Silvina Cohen Imach</h3>
+            <h4 className={styles["heading-quaternary"]}>Secretaria</h4>
+            <p>Silvina Cohen Imach</p>
           </div>
           <div>
-            <p>Prosecretaria</p>
-            <h3 className={styles["heading-tertiary"]}>Silvina Lis García</h3>
+            <h4 className={styles["heading-quaternary"]}>Prosecretaria</h4>
+            <p>Silvina Lis García</p>
           </div>
           <div>
-            <p>Tesorera</p>
-            <h3 className={styles["heading-tertiary"]}>Liliana Pernetti</h3>
+            <h4 className={styles["heading-quaternary"]}>Tesorera</h4>
+            <p>Liliana Pernetti</p>
           </div>
           <div>
-            <p>Protesorera</p>
-            <h3 className={styles["heading-tertiary"]}>María Aurelia López</h3>
+            <h4 className={styles["heading-quaternary"]}>Protesorera</h4>
+            <p>María Aurelia López</p>
           </div>
           <div>
-            <h3 className={styles["heading-tertiary"]}>Vocales Titulares</h3>
+            <h4 className={styles["heading-quaternary"]}>Vocales Titulares</h4>
             <p>
               Betria Alicia, Oscar Melillo, Ana Martos y Mula,Pontikos Mirna,
               Falocchi Andrea
             </p>
           </div>
           <div>
-            <h3 className={styles["heading-tertiary"]}>Vocales Suplentes</h3>
+            <h4 className={styles["heading-quaternary"]}>Vocales Suplentes</h4>
             <p>
               Fernando Castro, Levantini Susana, Laura Lesik,Cecilia Diez,
               Marcelo Alé
             </p>
           </div>
           <div>
-            <h3 className={styles["heading-tertiary"]}>Síndicos</h3>
-            <p>
-              Mercado Beatriz, CostanzaGabriela; <strong>Suplente:</strong>
-              Romina Colacci
-            </p>
+            <h4 className={styles["heading-quaternary"]}>Síndicos</h4>
+            <p>Mercado Beatriz, CostanzaGabriela; Suplente Romina Colacci</p>
           </div>
           <div>
-            <h3 className={styles["heading-tertiary"]}>Past President:</h3>
+            <h4 className={styles["heading-quaternary"]}>Past President</h4>
             <p>Mónica Guinzbourg</p>
           </div>
         </section>
