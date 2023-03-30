d99ff8444e5e27eeab3b97cb2e241a22b3f47640
j-leklere <jleklere@yahoo.com.ar>
2023-03-30 11:28:12 -0300

    Congresses styles edited

--- src/Pages/Congresos.jsx
+++ src/Pages/Congresos.jsx
@@ -14,60 +14,111 @@ function Congresos() {
             </h2>
             <h3 className={styles["heading-tertiary"]}>Un camino recorrido</h3>
             <div className={styles["congresses-timeline"]}>
-              <p>
-                XXIV Congreso Nacional de Psicodiagnóstico <br /> “El
-                psicodiagnóstico en la era digital: los vínculos en tiempos de
-                redes sociales” <br /> Córdoba, 2022
-              </p>
-              <p>
-                XXIII Congreso Nacional de Psicodiagnóstico <br />
-                “Psicodiagnóstico y cultura. Intervención en las nuevas
-                realidades” <br /> Santiago del Estero, 2019.
-              </p>
-              <p>
-                XXII Congreso Nacional de Psicodiagnóstico <br /> “La creación”
-                <br />
-                Rosario, 2018.
-              </p>
-              <p>
-                XXI Congreso Nacional de Psicodiagnóstico <br /> “Integrando
-                perspectivas en contextos diversos” <br /> CABA ,2017.
-              </p>
-              <p>
-                XX Congreso Nacional de Psicodiagnóstico <br /> “El
-                psicodiagnóstico y los desafíos actuales. Identificando
-                potencialiades” <br /> San Luis, 2016.
-              </p>
-              <p>
-                XIX Congreso Nacional de Psicodiagnóstico <br /> “Evaluando
-                vulnerabilidades en diferentes contextos” <br /> Santa Fe, 2015.
-              </p>
-              <p>
-                XVIII Congreso Nacional de Psicodiagnóstico <br />
-                “Psicodiagnóstico. Articulaciones interdisciplinarias” <br />
-                Mar del Plata, 2014.
-              </p>
-              <p>
-                XVII Congreso Nacional de Psicodiagnóstico <br /> “El
-                diagnóstico psicológico en la actualidad. Subjetividades,
-                contextos e instrumentos” <br /> C.A.B.A. , 2013.
-              </p>
-              <p>
-                XVI Congreso Nacional de Psicodiagnóstico <br /> “La
-                investigación en psicodiagnóstico. El vínculo en el origen y
-                desarrollo de la investigación” <br /> Rosario, 2012.
-              </p>
-              <p>
-                XV Congreso Nacional de Psicodiagnóstico <br />
-                “Psicodiagnóstico, demandas actuales, prevención” <br /> Tucumán
-                2011.
-              </p>
-              <p>
-                XIV Congreso Nacional de Psicodiagnóstico <br />
-                “Psicodiagnóstico. Culturas y evaluaciones” <br /> Corrientes,
-                2010.
-              </p>
-              <p>
+              <div className={styles.congress}>
+                <p className={styles["congress-number"]}>
+                  XXIV Congreso Nacional de Psicodiagnóstico
+                </p>
+                <p className={styles["congress-name"]}>
+                  “El psicodiagnóstico en la era digital: los vínculos en
+                  tiempos de redes sociales”
+                </p>
+                <p className={styles["congress-date"]}>Córdoba, 2022</p>
+              </div>
+              <div className={styles.congress}>
+                <p className={styles["congress-number"]}>
+                  XXIII Congreso Nacional de Psicodiagnóstico
+                </p>
+                <p className={styles["congress-name"]}>
+                  “Psicodiagnóstico y cultura. Intervención en las nuevas
+                  realidades”
+                </p>
+                <p className={styles["congress-date"]}>
+                  Santiago del Estero, 2019
+                </p>
+              </div>
+              <div className={styles.congress}>
+                <p className={styles["congress-number"]}>
+                  XXII Congreso Nacional de Psicodiagnóstico
+                </p>
+                <p className={styles["congress-name"]}> “La creación”</p>
+                <p className={styles["congress-date"]}>Rosario, 2018</p>
+              </div>
+              <div className={styles.congress}>
+                <p className={styles["congress-number"]}>
+                  XXI Congreso Nacional de Psicodiagnóstico
+                </p>
+                <p className={styles["congress-name"]}>
+                  “Integrando perspectivas en contextos diversos”
+                </p>
+                <p className={styles["congress-date"]}>CABA, 2017</p>
+              </div>
+              <div className={styles.congress}>
+                <p className={styles["congress-number"]}>
+                  XX Congreso Nacional de Psicodiagnóstico
+                </p>
+                <p className={styles["congress-name"]}>
+                  “El psicodiagnóstico y los desafíos actuales. Identificando
+                  potencialiades”
+                </p>
+                <p className={styles["congress-date"]}>San Luis, 2016</p>
+              </div>
+              <div className={styles.congress}>
+                <p className={styles["congress-number"]}>
+                  XIX Congreso Nacional de Psicodiagnóstico
+                </p>
+                <p className={styles["congress-name"]}>
+                  “Evaluando vulnerabilidades en diferentes contextos”
+                </p>
+                <p className={styles["congress-date"]}>Santa Fe, 2015</p>
+              </div>
+              <div className={styles.congress}>
+                <p className={styles["congress-number"]}>
+                  XVIII Congreso Nacional de Psicodiagnóstico
+                </p>
+                <p className={styles["congress-name"]}>
+                  “Psicodiagnóstico. Articulaciones interdisciplinarias”
+                </p>
+                <p className={styles["congress-date"]}>Mar del Plata, 2014</p>
+              </div>
+              <div className={styles.congress}>
+                <p className={styles["congress-number"]}>
+                  XVII Congreso Nacional de Psicodiagnóstico
+                </p>
+                <p className={styles["congress-name"]}>
+                  “El diagnóstico psicológico en la actualidad. Subjetividades,
+                  contextos e instrumentos”
+                </p>
+                <p className={styles["congress-date"]}>C.A.B.A., 2013</p>
+              </div>
+              <div className={styles.congress}>
+                <p className={styles["congress-number"]}>
+                  XVI Congreso Nacional de Psicodiagnóstico
+                </p>
+                <p className={styles["congress-name"]}>
+                  “La investigación en psicodiagnóstico. El vínculo en el origen
+                  y desarrollo de la investigación”
+                </p>
+                <p className={styles["congress-date"]}>Rosario, 2012</p>
+              </div>
+              <div className={styles.congress}>
+                <p className={styles["congress-number"]}>
+                  XV Congreso Nacional de Psicodiagnóstico
+                </p>
+                <p className={styles["congress-name"]}>
+                  “Psicodiagnóstico, demandas actuales, prevención”
+                </p>
+                <p className={styles["congress-date"]}>Tucumán, 2011</p>
+              </div>
+              <div className={styles.congress}>
+                <p className={styles["congress-number"]}>
+                  XIV Congreso Nacional de Psicodiagnóstico
+                </p>
+                <p className={styles["congress-name"]}>
+                  “Psicodiagnóstico. Culturas y evaluaciones”
+                </p>
+                <p className={styles["congress-date"]}>Corrientes, 2010</p>
+              </div>
+              <p className={styles["first-congress"]}>
                 El I Congreso Nacional de Psicodiagnóstico y VIII Jornadas
                 Nacionales de ADEIP, “Psicodiagnosticar. Un camino hacia la
                 prevención”, se realizó en San Martín de los Andes, 1997.
