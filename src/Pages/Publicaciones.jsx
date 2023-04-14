76ca8124b1f736aecdcc67eb31b8ac653aac637b
sleklere <santiagoleklere@gmail.com>
2023-04-14 10:37:53 -0300

    moved pdf to public folder

--- src/Pages/Publicaciones.jsx
+++ src/Pages/Publicaciones.jsx
@@ -4,14 +4,16 @@ import styles from "./Publicaciones.module.css";
 import backgroundImage from "../Assets/page-layout-top.jpg";
 import cardImg from "../Assets/publicaciones/brave_iM2sQbAyVY.png";
 
-import vol30 from "../Assets/publicaciones/PSICODIAGNOSTICAR-30.pdf";
-
 function Publicaciones() {
   return (
     <>
       <PageLayoutTop title={"PUBLICACIONES"} image={backgroundImage} />
       <div className={styles.cards}>
-        <Card cardImg={cardImg} title={"PSICODIAGNOSTICAR 30"} pdf={vol30} />
+        <Card
+          cardImg={cardImg}
+          title={"PSICODIAGNOSTICAR 30"}
+          pdf={process.env.PUBLIC_URL + "/PSICODIAGNOSTICAR-30.pdf"}
+        />
       </div>
     </>
   );
