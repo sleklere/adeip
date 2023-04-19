78e554ff87c2285ce31ffc7d6a81b94c3906c43f
sleklere <santiagoleklere@gmail.com>
2023-04-19 12:30:08 -0300

    Basic error page built

--- src/Pages/Error.jsx
+++ src/Pages/Error.jsx
@@ -1,3 +1,23 @@
-function ErrorPage() {}
+import { useRouteError } from "react-router-dom";
+import Header from "../Components/Header";
+import styles from "./Error.module.css";
+
+function ErrorPage() {
+  const error = useRouteError();
+  console.log(error);
+
+  return (
+    <>
+      <Header />
+      <main className={styles["error-content"]}>
+        <h1>¡Vaya! Algo salió mal.</h1>
+        <p>
+          <span>{error.status} </span>
+          {error.statusText}
+        </p>
+      </main>
+    </>
+  );
+}
 
 export default ErrorPage;
