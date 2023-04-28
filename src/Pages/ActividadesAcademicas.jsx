f45fe1438d892f18e930785dd8f09ffbb6c65095
Joaquin Leklere <jleklere@yahoo.com.ar>
2023-04-28 18:43:57 -0300

    Added new course

--- src/Pages/ActividadesAcademicas.jsx
+++ src/Pages/ActividadesAcademicas.jsx
@@ -3,6 +3,7 @@ import Course from "../Components/Course";
 import styles from "./ActividadesAcademicas.module.css";
 import imgCourse1 from "../Assets/actividadesAcademicas/course1.jpg";
 import imgCourse2 from "../Assets/actividadesAcademicas/course2.jpg";
+import imgCourse3 from "../Assets/actividadesAcademicas/course3.png";
 
 function ActividadesAcademicas() {
   return (
@@ -12,6 +13,14 @@ function ActividadesAcademicas() {
         <h2 className={styles["heading-secondary-actividades"]}>Cursos</h2>
         <div className={styles["cursos-container"]}>
           <Course
+            courseImg={imgCourse2}
+            courseName="Pascal Roman"
+            courseDate="28/04"
+            courseModality="Virtual"
+            courseIncriptionPartners="$3000"
+            courseIncriptionNoPartners="$4000"
+          />
+          <Course
             courseImg={imgCourse1}
             courseName="John Exner"
             courseDate="15/04"
@@ -20,12 +29,12 @@ function ActividadesAcademicas() {
             courseIncriptionNoPartners="$7000"
           />
           <Course
-            courseImg={imgCourse2}
-            courseName="Pascal Roman"
-            courseDate="28/04"
-            courseModality="Virtual"
-            courseIncriptionPartners="$3000"
-            courseIncriptionNoPartners="$4000"
+            courseImg={imgCourse3}
+            courseName="Psicodiagnóstico de Rorschach"
+            courseDate="01/04"
+            courseModality="Presencial"
+            courseIncriptionPartners="$5000"
+            courseIncriptionNoPartners="$7000"
           />
         </div>
       </main>
