54c44ea2d3c4621283f67e7fb6cf1fb91b76c58a
j-belec <jeronimobelec@hotmail.com>
2024-01-09 12:48:10 -0300

    boletines 2022/2023

--- src/Pages/Publicaciones.jsx
+++ src/Pages/Publicaciones.jsx
@@ -5,6 +5,8 @@ import backgroundImage from "../Assets/page-layout-top.jpg";
 import cardImg30 from "../Assets/publicaciones/PSICODIAGNOSTICAR-30.png";
 import cardImg29 from "../Assets/publicaciones/PSICODIAGNOSTICAR-29.png";
 import cardImg28 from "../Assets/publicaciones/PSICODIAGNOSTICAR-28.png";
+import cardImgBoletin22 from "../Assets/publicaciones/BoletinADEIP2022.jpeg";
+import cardImgBoletin23 from "../Assets/publicaciones/BoletinADEIP2023.jpeg";
 
 function Publicaciones() {
   return (
@@ -25,6 +27,21 @@ function Publicaciones() {
           cardImg={cardImg28}
           title={"PSICODIAGNOSTICAR 28"}
           pdf={process.env.PUBLIC_URL + "/PSICODIAGNOSTICAR-28.pdf"}
+        />
+      </div>
+      <div className={styles["boletines-h2-container"]}>
+        <h2 className={styles["boletines-h2"]}>Boletines</h2>
+      </div>
+      <div className={styles.cards}>
+        <Card
+          cardImg={cardImgBoletin22}
+          title={"Boletín ADEIP 2022"}
+          pdf={process.env.PUBLIC_URL + "/BoletinADEIP2022.pdf"}
+        />
+        <Card
+          cardImg={cardImgBoletin23}
+          title={"Boletín ADEIP 2023"}
+          pdf={process.env.PUBLIC_URL + "/BoletinADEIP2023.pdf"}
         />
       </div>
     </>
